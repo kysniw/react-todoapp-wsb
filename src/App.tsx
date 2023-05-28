d0c181277c91659de20a7aec2103bf322c00b77c
kysniw <kuba.winsyk.forgames@gmail.com>
2023-05-28 21:49:21 +0200

    Added responsive views and theme change logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./App.css";
 import AddTaskBar from "./components/AddTaskBar";
 import TasksList from "./components/TasksList";
@@ -9,14 +9,22 @@ import ThemeButton from "./components/ThemeButton";
 function App() {
   const { getTasks } = useContext(TasksContext);
 
+  const [isDark, setIsDark] = useState<boolean>(true);
+
   useEffect(() => {
     getTasks();
   }, [getTasks]);
 
+  const handleThemeChange = (isDarkHandled: boolean) => {
+    setIsDark(isDarkHandled);
+  };
+
+  const themeClass = isDark ? "--dark-mode" : "--light-mode";
+
   return (
-    <div className="app-container app-container--dark-mode">
-      <ThemeButton />
-      <div className="todo-app">
+    <div className={"app-container app-container" + themeClass}>
+      <ThemeButton isDark={isDark} onDarkThemeChange={handleThemeChange} />
+      <div className={"todo-app todo-app" + themeClass}>
         <AddTaskBar />
         <TasksList />
       </div>
--- src/components/ThemeButton.tsx
+++ src/components/ThemeButton.tsx
@@ -2,11 +2,29 @@ import React from "react";
 import "./ThemeButton.css";
 import { FaSun, FaMoon } from "react-icons/fa";
 
-function ThemeButton() {
+type Props = {
+  isDark: boolean;
+  onDarkThemeChange: (isDarkHandled: boolean) => void;
+};
+
+function ThemeButton({ isDark, onDarkThemeChange }: Props) {
+  const btnLightClass = isDark ? "" : " theme-button__light--active";
+  const btnDarkClass = isDark ? " theme-button__dark--active" : "";
+
   return (
-    <div className="theme-button">
-      <FaSun />
-      <FaMoon />
+    <div className="theme-button-container">
+      <div
+        onClick={() => onDarkThemeChange(false)}
+        className={"theme-button__light theme-btn" + btnLightClass}
+      >
+        <FaSun className="icon-sun" />
+      </div>
+      <div
+        onClick={() => onDarkThemeChange(true)}
+        className={"theme-button__dark theme-btn" + btnDarkClass}
+      >
+        <FaMoon className="icon-dark" />
+      </div>
     </div>
   );
 }
